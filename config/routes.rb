d53b59a5a5321e6d1cb3120894d4d7db11937882
a.minami <ruby.studying140@gmail.com>
2018-08-26 02:28:20 +0000

    CRUD tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# get 'messages/:id', to: 'messages#show'
+# post 'messages', to: 'messages#create'
+# put 'messages/:id', to: 'messages#update'
+# delete 'messages/:id', to: 'messages#destroy'
+
+# get 'messages', to: 'messages#index'
+# get 'messages',to: 'messages#new'
+# get 'messages/:id/edit', to: 'messages#edit'
+root to: 'tasks#index'
+
+resources :tasks
 end
