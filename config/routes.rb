17d808e2fbe84d48f91c5c512fcecf5189c29711
a.minami <ruby.studying140@gmail.com>
2018-08-27 00:27:23 +0000

    change index, new, controllerfiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-# get 'messages/:id', to: 'messages#show'
-# post 'messages', to: 'messages#create'
-# put 'messages/:id', to: 'messages#update'
-# delete 'messages/:id', to: 'messages#destroy'
+# get 'tasks/:id', to: 'tasks#show'
+# post 'tasks', to: 'tasks#create'
+# put 'tasks/:id', to: 'tasks#update'
+# delete 'tasks/:id', to: 'tasks#destroy'
 
-# get 'messages', to: 'messages#index'
-# get 'messages',to: 'messages#new'
-# get 'messages/:id/edit', to: 'messages#edit'
+# get 'tasks', to: 'tasks#index'
+# get 'tasks',to: 'tasks#new'
+# get 'tasks/:id/edit', to: 'tasks#edit'
 root to: 'tasks#index'
 
 resources :tasks
